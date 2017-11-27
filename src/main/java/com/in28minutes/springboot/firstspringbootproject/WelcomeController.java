4de899aa727977fe75c4ee27cd43acc8ba93a84b
Govind <shremish@indiana.edu>
2017-11-27 17:51:33 -0500

    added dependent class

--- src/main/java/com/in28minutes/springboot/firstspringbootproject/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/firstspringbootproject/WelcomeController.java
@@ -1,14 +1,27 @@
 package com.in28minutes.springboot.firstspringbootproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-		
+	
+	
+	@Autowired
+	SayHello Hello;
+	
 	@RequestMapping("/welcome")
 	public String Welcome() {
-		return "Hello World of Microsrvices";
+		return Hello.message();
 	}
 	
+}
+
+@Component
+class SayHello{
+	public String message() {
+		return "The World of microservices is really cool! updated";
+	}
 }
