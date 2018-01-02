1d2a578f52758d50293dda8f0ef1cb4dda09efb9
Govind <shremish@indiana.edu>
2018-01-02 07:55:17 -0500

    adding service in application.properties

--- src/main/java/com/in28minutes/springboot/controller/Welcome.java
+++ src/main/java/com/in28minutes/springboot/controller/Welcome.java
@@ -1,13 +1,16 @@
 package com.in28minutes.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Welcome {
 
+	@Value("${welcome.message}")
+	private String welcomeMessage;
 	@RequestMapping("/welcome")
 	public String showMessgae() {
-		return "This one is also working";
+		return welcomeMessage;
 	}
 }
