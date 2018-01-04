cac2711a337d677617f1f5c5d85ba0e6f2a28a82
Govind <shremish@indiana.edu>
2018-01-03 22:29:53 -0500

    adding methods in UserRepository

--- src/main/java/com/in28minutes/springboot/jpa/UserRepository.java
+++ src/main/java/com/in28minutes/springboot/jpa/UserRepository.java
@@ -1,8 +1,11 @@
 package com.in28minutes.springboot.jpa;
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends	CrudRepository<User, Long> {
 
+	List<User> findByName(String name);
 }
