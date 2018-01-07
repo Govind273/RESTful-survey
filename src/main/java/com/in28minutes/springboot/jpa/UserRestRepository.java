88d6ab73e5247ec1531d57f18bd53d367cfda73e
Govind <shremish@indiana.edu>
2018-01-07 04:59:40 -0500

    adding RestRepository methods

--- src/main/java/com/in28minutes/springboot/jpa/UserRestRepository.java
+++ src/main/java/com/in28minutes/springboot/jpa/UserRestRepository.java
@@ -4,8 +4,11 @@ package com.in28minutes.springboot.jpa;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface UserRestRepository extends	CrudRepository<User, Long> {
+@RepositoryRestResource(path="users",collectionResourceRel="users")
+public interface UserRestRepository extends	PagingAndSortingRepository<User, Long> {
 
 	List<User> findByName(String name);
 }
